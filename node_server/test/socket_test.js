1d80268268d19e542fa438cdef74da69d0dca3de
Jay Vishaal J <jayvishaalj14@gmail.com>
2020-08-03 06:39:11 +0530

    flask trail

--- node_server/test/socket_test.js
+++ node_server/test/socket_test.js
@@ -5,6 +5,6 @@ let socket = io.connect("http://13.234.75.146:5000/");
 socket.on('message',(data) => {
     console.log("recieved : ",data);
 });
-socket.on('1595785935038',(dataJson) => {
+socket.on('1596270583357',(dataJson) => {
     console.log("DATA FROM OTHER CLIENT : ",dataJson)
 });
